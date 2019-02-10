6fe61864dd390bb3c7e84195056b43da2e05b8dd
Matheus Marques <matheus_maghost@hotmail.com>
2019-02-10 21:08:34 -0200

    CRUD de categorias

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -4,6 +4,15 @@ module.exports = app => {
         .get(app.api.user.get)
 
     app.route('/users/:id')
-        .put(app.api.user.save)
         .get(app.api.user.getById)
+        .put(app.api.user.save)
+
+    app.route('/categories')
+        .post(app.api.category.save)
+        .get(app.api.category.get)
+
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
