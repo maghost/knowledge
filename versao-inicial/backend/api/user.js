ee628b3c8d85f0e3ef62912a50347a46ca8e8a45
Matheus Marques <matheus_maghost@hotmail.com>
2019-02-11 22:17:38 -0200

    Liberar cadastro de usuário administrador apenas para administradores

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -12,6 +12,10 @@ module.exports = app => {
         const user = { ...req.body }
         if (req.params.id) user.id = req.params.id
 
+        if (!req.originalUrl.startsWith('/users') || !req.user || !req.user.admin) {
+            user.admin = false
+        }
+
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
